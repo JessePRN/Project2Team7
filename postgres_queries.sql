5034104eb2da7d889916768cbe8b7f2f4b4502ef
Jesse Perez <jesseprn@gmail.com>
2022-06-08 21:34:21 -0400

    Queries used in postgres for join

---
+++ postgres_queries.sql
@@ -0,0 +1,15 @@
+-- our tables were created from our jupiter notebook, us_fuel_prices_etl
+
+-- check us_gas_price table
+-- select * from car_mpg
+
+-- check car_mpg table
+-- select * from car_mpg
+
+-- Joins tables
+SELECT *
+FROM car_mpg
+JOIN us_gas_price
+ON car_mpg."Year" = us_gas_price."Year";
+
+
